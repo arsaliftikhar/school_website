73059b76a260c42f4bf917b87500dc348d08bce2
arsaliftikhar <arsaliftikhar2@gmail.com>
2021-01-12 23:40:37 +0500

    second commit

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 const counters = document.querySelectorAll('.stat_num');
-const speed = 300; // The lower the slower
+const speed = 200; // The lower the slower
 
 counters.forEach(counter => {
 	const updateCount = () => {
@@ -25,3 +25,52 @@ counters.forEach(counter => {
 
 	updateCount();
 });
+
+
+$('#toggle_button').click(function () {
+	$('.list_toggle').fadeToggle();
+});
+
+
+ $(document).ready(function(){
+	$(window).on('scroll',function(){
+	$('#remove_nav_clr').css('background-color','#222222');
+	});
+});
+/*scrolling*/
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+		// On-page links
+		if (
+			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+			&&
+			location.hostname == this.hostname
+		) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000, function() {
+					// Callback after animation
+					// Must change focus!
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) { // Checking if the target was focused
+						return false;
+					} else {
+						$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+						$target.focus(); // Set focus again
+					};
+				});
+			}
+		}
+	});
